6d2d7bfba0c9956b2af3fdf09f5b6d0c902106d2
PRADIP BHATT <pradip10bhatt@gmail.com>
2024-02-27 10:27:07 +0545

    second version

---
+++ REAME.md
@@ -0,0 +1,7 @@
+You may contribute in this repo.
+add 4 basic extesnions to run these codes
+1)c/c++ extesions from microsoft
+2)code runner
+3)Exe runner
+to make code executable run this in terminal 
+g++ filename.c -o filename.exe -lbgi -lgdi32 -lcomdlg32 -luuid -loleaut32 -lole32
---
+++ mid-pointcircle.cpp
@@ -0,0 +1,61 @@
+#include <graphics.h>
+#include <iostream>
+
+class MidpointCircle {
+private:
+    int x_center, y_center, radius;
+
+public:
+    MidpointCircle(int x, int y, int r) : x_center(x), y_center(y), radius(r) {}
+
+    void drawCircle() {
+        int x = radius;
+        int y = 0;
+        int p = 1 - radius;
+
+        while (x > y) {
+            putPixel(x + x_center, y + y_center);
+            putPixel(y + x_center, x + y_center);
+            putPixel(-y + x_center, x + y_center);
+            putPixel(-x + x_center, y + y_center);
+            putPixel(-x + x_center, -y + y_center);
+            putPixel(-y + x_center, -x + y_center);
+            putPixel(y + x_center, -x + y_center);
+            putPixel(x + x_center, -y + y_center);
+
+            y++;
+
+            if (p <= 0)
+                p = p + 2 * y + 1;
+            else {
+                x--;
+                p = p + 2 * y - 2 * x + 1;
+            }
+        }
+    }
+
+private:
+    void putPixel(int x, int y) {
+        putpixel(x, y, WHITE);
+    }
+};
+
+int main() {
+    int x, y, r;
+    std::cout << "Enter the x-coordinate of the center: ";
+    std::cin >> x;
+    std::cout << "Enter the y-coordinate of the center: ";
+    std::cin >> y;
+    std::cout << "Enter the radius of the circle: ";
+    std::cin >> r;
+
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, NULL);
+
+    MidpointCircle circle(x, y, r);
+    circle.drawCircle();
+
+    delay(5000);
+    closegraph();
+    return 0;
+}
